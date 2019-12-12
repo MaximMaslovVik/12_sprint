7ca8cd54c8b65762754e02464d2b4bf7a9fdaae1
Максим Маслов <flyover31@yandex.ru>
2019-12-12 20:05:59 +0300

    убрал часть файлов относящиеся к вэбпаку

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const { PORT = 3000 } = process.env;
 const app = express();
 app.use(express.static(__dirname + '/public'));
 app.use('/', users, cards);
+app.get('/*', (req, res) => {
+  res.status(404).send({ "message": "Запрашиваемый ресурс не найден" });
+});
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
-});
-app.get('/*', (req, res) => {
-  res.statusCode = 404;
-  res.statusMessage = 'Error';
-  res.send({ "message": "Запрашиваемый ресурс не найден" });
 });
