7686156683ac0f40b8e335452b52b291cd56c930
Максим Маслов <flyover31@yandex.ru>
2019-12-13 19:14:11 +0300

    сервер запускается на 2 ветках, убрал из пекидж файла почти весь вебпак

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const path = require('path');
 const users = require('./routes/users.js');
 const cards = require('./routes/cards.js');
+
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/', users);
+app.use('/', cards);
+app.listen(PORT, () => {});
 
-app.use(express.static(__dirname + '/public/dist'));
-app.use('/', users, cards);
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
+app.get('/*', (req, res) => {
+  res.statusCode = 404;
+  res.statusMessage = 'Error';
+  res.send({ message: 'Запрашиваемый ресурс не найден' });
 });
