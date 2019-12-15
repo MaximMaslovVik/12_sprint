c23081b18214599a64462138c3252c0b58ec35b9
Максим Маслов <flyover31@yandex.ru>
2019-12-15 20:32:59 +0300

    изменил

---
+++ routes/users.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const fs = require('fs');
+
+router.get('/users', (req, res) => {
+  fs.readFile('data/users.json', 'utf8', (error, data) => {
+    if (error) throw error;
+    res.send(data);
+  });
+});
+
+router.get('/users/:id', (req, res) => {
+  const idUser = req.params.id;
+  fs.readFile('data/users.json', 'utf8', (error, data) => {
+    const cityId = JSON.parse(data).find((us) => us._id === idUser);
+    if (cityId) {
+      res.send(cityId);
+    } else if (!cityId) {
+      res.statusCode = 404;
+      res.statusMessage = 'Error';
+      res.send({ message: 'Такого пользователя нет' });
+    }
+  });
+});
+
+module.exports = router;
