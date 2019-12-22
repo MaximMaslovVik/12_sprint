8de9e420c88aa4dc0f8b55538c66e04ba7f143c9
Максим Маслов <flyover31@yandex.ru>
2019-12-23 00:44:36 +0300

    добавил ошибку при чтении файла

--- routes/users.js
+++ routes/users.js
@@ -15,11 +15,14 @@ router.get('/users/:id', (req, res) => {
   const idUser = req.params.id;
   fs.readFile('data/users.json', 'utf8', (error, data) => {
     if (error != null) {
+      return res.status(500).send({ message: 'На сервере произошла ошибка' });
+    }
+    const user = JSON.parse(data).find((us) => {
+      if (us._id != idUser){
       return res.status(404).send({ message: 'Такого пользователя нет' });
     }
-    // eslint-disable-next-line no-underscore-dangle
-    const user = JSON.parse(data).find((us) => us._id === idUser);
     return res.send({ data: user });
+   });
   });
 });
 
