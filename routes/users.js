4d41c3d722ea42c55de68c22dfd40a46e7e199f2
Максим Маслов <flyover31@yandex.ru>
2019-12-23 20:00:36 +0300

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -18,12 +18,11 @@ router.get('/users/:id', (req, res) => {
       return res.status(500).send({ message: 'На сервере произошла ошибка' });
     }
     const user = JSON.parse(data).find((us) => {
-      if (us._id === idUser){
-        return res.send({ data: user });
+      if (! idUser){
+        return res.status(404).send({ message: 'Такого пользователя нет' });
       }
-      return res.status(404).send({ message: 'Такого пользователя нет' });
     });
-
+    return res.send({ data: user });
   });
 });
 
